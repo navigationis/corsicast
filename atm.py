967d3f261e512e51490186b689fbed187c8a4344
Kael D. HANSON <kael@navigation.is>
2023-12-26 09:22:24 -0600

    Complete refactor of atm gen + temp reco.

--- atm.py
+++ atm.py
@@ -1,19 +1,262 @@
+"""
+Atmosphere module.
+"""
+
 import numpy as np
 from scipy.interpolate import UnivariateSpline
-from scipy.integrate import solve_ivp
+from scipy.integrate import RK23, OdeSolution
 from datetime import datetime
 
 from MCEq.geometry.density_profiles import EarthsAtmosphere
+
+M_m = 0.028966  
+R   = 8.314
+g   = 9.80665   
+
+def dPdh(h, p, temperature):
+    """
+    Change of pressure with respect to height. Units are MKS.
+    Used for constructing atmospheres from temperature profiles
+    through solution of the differential equation though :func:`scipy.integrate.solve_ivp`
+
+    Parameters
+    ----------
+    h : float or numpy.ndarray-like
+        Geopotential height in meters.
+    p : float 
+        Pressure
+    temperature : {function}
+        Function that provide temperature, in Kelvin, versus geopotential height
+    """
+    return -p * M_m * g / (R * temperature(h))
+
+class AtmospherefromTemperatureProfile(EarthsAtmosphere):
+    """
+    Atmosphere created from temperature profile.
+
+    Parameters
+    ----------
+    alt : numpy.ndarray or list or float
+        Geopotential altitudes, in meters, of temperature points.
+    T : numpy.ndarray or list or float
+        Temperature, in Kelvin, at sample points.
+    P0 : float
+        Reference pressure, in Pa.
+    isa_fill : bool
+        If True, fill missing values above maximum altitude specified
+        in alt from the ISA standard values.
+    spline_k : int
+        Degree of spline interpolation for T spline
+    spline_s : float
+        Smoothness parameter for spline interpolation
+
+    Notes
+    -----
+    If `alt` or `T` are omitted and isa_fill is set to True then the
+    atmosphere created is the International Standard Atmosphere (ISA).
+
+    Examples
+    --------
+    Define the temperature at a few points in the troposphere and let
+    the constructor fill in the ISA above (in this case beginning at 11 km):
+    >>> z = (1000, 2000, 10000)
+    >>> T = (10, 5, -20)
+    >>> a = atm.fromTemperatureProfile(alt=z, T=T, P0=90000., isa_fill=True)
+
+    One possible use case where one desires to use the ISA for most of the
+    atmosphere but perhaps wants to change the sea level temperature and 
+    pressure (here 30.00" Hg):
+    >>> a = atm.fromTemperatureProfile(T=25, P0=101596, isa_fill=True)
+    """
+    _isa = np.array((
+        ( -610,  19.0), (11000, -56.5), (20000, -56.5), 
+        (32000, -44.5), (47000,  -2.5), (51000,  -2.5),
+        (71000, -58.5), (84852, -86.28)), dtype = 'd')
+    
+    def __init__(
+            self, 
+            alt: list = None, 
+            T: list = None, 
+            P0: float = 108900.,
+            isa_fill: bool = False, 
+            spline_k: int = 1,
+            spline_s: float = 0):
+        
+        super().__init__(self)
+
+        if T is None:
+            if not isa_fill: raise ValueError('Empty temperature profile')
+            self.profile = self._isa
+        else:
+            if alt is None: alt = 0.0
+            alt = np.atleast_1d(alt)
+            T   = np.atleast_1d(T)
+            self.profile = np.stack((alt, T), -1)
+            if isa_fill: 
+                self.profile = np.concatenate((
+                    self.profile,
+                    np.array(list(filter(lambda x: x[0] > alt[-1], self._isa)))
+                    ))
+                
+        self.P0 = P0
+        self._Tspline = UnivariateSpline(
+            self.profile[:,0], self.profile[:,1] + 273.16,
+            k = spline_k, s = spline_s, ext = 'const')
+        
+        z0 = self.profile[0,0]
+        rk = RK23(lambda h, p: dPdh(h, p, self._Tspline), 
+                  z0, np.atleast_1d(self.P0), 100000.0)
+        
+        z = [z0]
+        interpolants = []
+        while rk.status == 'running':
+            rk.step()
+            z.append(rk.t)
+            interpolants.append(rk.dense_output())
+
+        self._p = (z, interpolants)
+        
+        self._pressure_solution = OdeSolution(z, interpolants)
+
+    def pressure(self, alt: float | list | np.ndarray):
+        """
+        Return pressure, in Pascal, given altitude in meters.
+
+        Parameters
+        ----------
+        alt : float or list of floats
+            Altitude in meters
+
+        Returns
+        -------
+        pa : float or list of floats
+            Pressure, in Pa.
+        """
+        return self._pressure_solution(alt).flatten()
+    
+    def temperature(self, alt: float | list | np.ndarray):
+        """ 
+        Return temperature, in Kelvin, given altitude in meters.
+
+        Parameters
+        ----------
+        alt : float or list of floats
+            Altitude in meters.
+
+        Returns
+        -------
+        T : float or list of floats
+            Temperature, in K.
+        """
+        return self._Tspline(alt)
+    
+    def thickness(self, alt: float):
+        """ 
+        Return thickness, in kg/m^2.
+        """
+        return self.pressure(alt) / g
+    
+    def density(self, alt: float):
+        """ 
+        Return density, in kg/m^3
+        """
+        return self.pressure(alt) * 0.028966 / (8.314 * self._Tspline(alt))
+    
+    def get_density(self, h_cm: float) -> float:
+        """
+        Overriden density function from MCEq.geometry.density_profiles.EarthsAtmosphere.
+        Returns density in g/cm^3 units given a height h_cm in centimeters.
+
+        Parameters
+        ----------
+        h_cm : float
+            Height, in cm.
+
+        Returns
+        -------
+        rho : float
+            Density at h_cm, in g/cm^3
+        """
+        z = h_cm / 100.0
+        if z > 100000.0: return 1E-15
+        return self.density(z) * 0.001
+
+
+def generate_atmosphere(
+        alt: float = 0.0, 
+        P0: float = 101325.,
+        temp_a: float = 8.0,
+        temp_b: float = 85.):
+    """
+    Create Atmosphere by generating random temperature profile.
+
+    Parameters
+    ----------
+    alt : float
+        Base (i.e. ground level) altitude, in meters.
+    P0 : float
+        Ground level pressure, in Pa
+    temp_a: float
+        Weibull a parameter for ground level temperature randomization.
+        Smaller numbers will broaden the distribution.
+    temp_b: float
+        Weibull width parameter for ground level temperature randomization.
+        Larger numbers will broaden the distribution.
+    """
+    atm_par = (((  500, 1000), (-0.0075, 0.0025)), # troposphere - 1
+               (( 2500, 8000), (-0.0080,-0.0045)), # troposphere - 2
+               (( 4000, 9000), (-0.0080,-0.0045)), # troposphere - 3
+               (( 1000, 4000), (-0.0050, 0.0025)), # troposphere - 4
+               ((  500,15000), (-0.0010, 0.0010)), # tropopause - 1
+               ((12000,20000), ( 0.0000, 0.0035)), # stratosphere - 1
+               (( 1000, 8000), (-0.0002, 0.0002)), # stratopause
+               ((15000,25000), (-0.0025,-0.0020)), # mesosphere - 1
+               (( 5000,10000), (-0.0001, 0.0001)), # mesopause
+               ((10000,20000), (-0.0001, 0.0035)), # thermosphere
+               ((10000,20000), ( 0.0000, 0.0045)))
+    
+    z = [alt]
+    T = [np.random.weibull(temp_a)*temp_b - 60]
+
+    # build up the atmosphere - first layer might have a temperature inversion
+    for (dz0, dz1), (dTdz0, dTdz1) in atm_par:
+        z.append(z[-1] + np.random.uniform(dz0, dz1))
+        dz = z[-1] - z[-2]
+        T.append(T[-1] + np.random.uniform(dTdz0, dTdz1)*dz)
+
+    return AtmospherefromTemperatureProfile(z, T, P0 = P0)
+
+def read_igra_station_list(filename:str):
+    """
+    Reader for the IGRA v2.2 station list (updated daily).
+    Returns a record array encapsulating data in the IGRA station list.
+    """
+    dtype = [('station_id', 'U11'), ('latitude', float), ('longitude', float), 
+             ('elevation', float), ('state', 'U2'), ('name', 'U29'), 
+             ('firstyear', int), ('lastyear', int), ('nobs', int)]
+    with open(filename, 'rt') as f:
+        ra = []
+        for line in f.readlines():
+            ra.append((line[0:11], float(line[12:20]), float(line[21:30]), 
+                       float(line[31:37]), line[38:40], line[41:71], 
+                       int(line[72:76]), int(line[77:81]), int(line[82:88])))
+    return np.array(ra, dtype)
 
 class Rawinsonde:
     """
     Class for handling rawinsondes or radiosondes data.
     [NCEI](https://www.ncei.noaa.gov/products/weather-balloon/integrated-global-radiosonde-archive) 
-    archives data which can be directly read by :func:read_station_data. 
+    archives data which can be directly read by `read_station_data`. 
     Normally you should not need to instantiate this class directly but rather 
-    retrieve observations from the archive.
+    retrieve observations from the archive using `read_station_data` or
+    generate simulated observations.
     """
-    def __init__(self, hdr:str):
+    _dtype=[('lvltyp', 'i2'), ('etime', 'i4'),
+            ('pressure', np.float32), ('gph', np.float32), ('temperature', np.float32), 
+            ('rh', np.float32), ('dewpoint', np.float32), ('winddir', np.float32),
+            ('windspeed', np.float32)]
+    
+    def __init__(self, hdr: str):
         self.st_id = hdr[1:13]
         self.dtime = datetime(int(hdr[13:17]), int(hdr[18:20]), int(hdr[21:23]), int(hdr[24:26]))
         self.numlev = int(hdr[32:36])
@@ -21,14 +264,11 @@ class Rawinsonde:
         self.np_src = hdr[46:54]
         self.lat    = float(hdr[55:62])/10000.
         self.lon    = float(hdr[63:71])/10000.
-        self.obs = np.zeros(self.numlev, dtype=[('lvltyp', 'i8'), ('etime', 'i4'),
-            ('pressure', np.float32), ('gph', np.float32), ('temperature', np.float32), 
-            ('rh', np.float32), ('dewpoint', np.float32), ('winddir', np.float32),
-            ('windspeed', np.float32)])
+        self.obs = np.zeros(self.numlev, dtype=Rawinsonde._dtype)
         self.__iobs = 0
         self.invalid  = None
 
-    def add_observation(self, obstxt:str):
+    def add_observation(self, obstxt: str):
         i = self.__iobs
         self.__iobs += 1
         self.obs[i] = (int(obstxt[0:2]), int(obstxt[3:8]), float(obstxt[9:15])/100.,
@@ -37,22 +277,6 @@ class Rawinsonde:
         if self.__iobs == self.numlev: self.invalid = (self.obs['pressure'] < 0.0) | \
             (self.obs['temperature'] < -100.0) | \
             (self.obs['gph'] == -9999.0)
-
-    @property
-    def observation(self):
-        return self.obs.view(np.recarray)
-
-    @property
-    def pressure(self):
-        return ma.masked_array(self.obs['pressure'], mask=self.obs['pressure']<0.0)
-
-    @property
-    def temperature(self):
-        return ma.masked_array(self.obs['temperature'], mask=self.obs['temperature']==-999.9)
-
-    @property
-    def geopotential_height(self):
-        return ma.masked_array(self.obs['gph'], mask=self.obs['pressure']<0.0)
 
     @property
     def density(self):
@@ -66,6 +290,16 @@ def read_station_data(filename: str) -> Rawinsonde:
     """
     Read [NCEI](https://www.ncei.noaa.gov/products/weather-balloon/integrated-global-radiosonde-archive) 
     radiosonde data archives.
+
+    Parameters
+    ----------
+    filename : str
+        Path to raw text file holding observation data.
+        
+    Returns
+    -------
+    rws : Rawinsonde
+
     """
     raw = None
     i = 0
@@ -80,68 +314,28 @@ def read_station_data(filename: str) -> Rawinsonde:
             else:
                 raw.add_observation(line)
 
-def dPdh(h, p, temperature):
-    M_m = 0.028966 
-    R   = 8.314
-    g   = 9.80
-    return -p * M_m * g / (R * temperature(h))
+def generate_station_data(
+        n: int,
+        st_id: str,
+        alt: float | None = None,
+        SLP: float | None = None
+        ):
+    """
+    Simulate rawinsonde observations.
+
+    Paramters
+    ---------
+    n : int
+        Number of observations.
+    st_id : str
+        Station ID.
+        Can be any string, IGRA format is not enforced.
+    alt : float
+        Station altitude
+
     
-class RWSAtmosphere(EarthsAtmosphere):
-    def __init__(self, rws_obs):
-        super().__init__(self)
-        self.rws = rws_obs
 
-        # Setup the atmospheric density calculator
-        gph = self.rws.valid.gph
-        tmp = self.rws.valid.temperature
-        P0  = self.rws.valid.pressure[0] * 100
-        upper_z = np.array((48000, 54000, 61000, 80000, 90000, 100000), 'd')
-        upper_T = np.array((0, 0, -10, -80, -80, -65), 'd')
-        z = np.concatenate((gph, upper_z))
-        T = np.concatenate((tmp, upper_T)) + 273.16
-        self._spline_T = UnivariateSpline(z, T, s=25, ext='const')
-        t_eval = np.concatenate((np.arange(0, 10000, 250, dtype='d'),
-                                 np.arange(10000, 30000, 500, dtype='d'),
-                                 10**np.linspace(4.475, 5, 50)))
-        self.psoln = solve_ivp(dPdh, (0, 1E5), y0=(P0,), t_eval = t_eval, args=(self._spline_T,))
-        self._spline_P = UnivariateSpline(self.psoln.t, self.psoln.y[0], k=1, s=0, ext='const')
-
-    def get_density(self, h_cm):
-        z = h_cm / 100.0
-        P = self._spline_P(z)
-        T = self._spline_T(z)
-        return P * 0.028966 / (8.314 * T) * 0.001
-    
-class RWSim:
-    def __init__(self):
-        self.obs = np.zeros(5, dtype=[('gph', 'd'), ('temperature', 'd'), ('pressure', 'd')])
-
-        a0 = np.random.uniform(0, 1)
-        a1 = np.random.exponential(scale=150)
-        a2 = np.random.normal(loc=1250, scale=250)
-        a3 = np.random.uniform(0, 3000)
-        
-        self.obs['gph'][0] = np.where(a0 > 0.95, a3, np.where(a0 > 0.88, a2, a1))
-        self.SLP = np.random.normal(loc=29.92, scale=0.5)
-    
-        self.obs['pressure'][0] = self.SLP * 33.87 * ((288.16 - 0.0065*self.obs['gph'][0])/288.16)**5.26
-        
-        self.obs['temperature'][0] = np.random.weibull(12)*85 - 60
-
-        lapse_1 = -0.0065 + np.random.uniform(-0.0015, 0.0015)
-        self.obs['gph'][1]  = np.random.uniform(8000, 15000)
-        self.obs['temperature'][1] = self.obs['temperature'][0] + self.obs['gph'][1]*lapse_1
-
-        lapse_2 = np.random.uniform(-0.0005, 0.0005)
-        self.obs['gph'][2]  = self.obs['gph'][1] + np.random.uniform(0, 5000)
-        self.obs['temperature'][2] = self.obs['temperature'][1] + self.obs['gph'][2]*lapse_2
-
-        lapse_3 = np.random.uniform(-0.001, 0.001)
-        self.obs['gph'][3]  = self.obs['gph'][2] + np.random.uniform(2000, 5000)
-        self.obs['temperature'][3] = self.obs['temperature'][2] + self.obs['gph'][3]*lapse_3
-
-        lapse_4 = np.random.uniform(0.00025, 0.002)
-        self.obs['gph'][4]  = 35000.0
-        self.obs['temperature'][4] = self.obs['temperature'][3] + (self.obs['gph'][4] - self.obs['gph'][3])*lapse_4
-
-        self.valid = np.rec.array(self.obs)
+    """
+    hdr = f"#{id:11s} {year:4d} {month:2d} {day:2d} {hour:2d} 9999" + \
+        f" 0005 ncdc-nws ncdc-nws {int(lat*10000):7d} {int(lon*10000):7d}"
+    return Rawinsonde(hdr)
--- scripts/patgen.py
+++ scripts/patgen.py
@@ -1,31 +1,65 @@
 import numpy as np
-from atm import RWSim, RWSAtmosphere
+from atm import generate_atmosphere
 from concurrent.futures import ProcessPoolExecutor
 import mceq_config
 from MCEq.core import MCEqRun
 import crflux.models as crf
 
-def simulate_atmosphere(r, theta, mceq=None):
+def simulate_atmosphere(a, theta, mceq=None, alt_grid=None, progress=None):
+    """
+    Execute MCEq simulation of one atmosphere, a, over angles theta.
 
+    Parameters
+    ----------
+    a : EarthsAtmosphere
+        Atmosphere
+    theta : float or list
+        Zenith angles
+    mceq : MCEqRun or None
+        MCEq kernel. If None one will be created within this function. This 
+        latter case may be useful for multiprocessing, if I can ever get it
+        to work.
+    alt_grid : list of floats
+        Altitude grid to solve fluxes on - must be transformed to an X grid
+        by the rotated Atmosphere.
+    progress : tqdm or None
+        Progress bar a la tqdm
+    """
     if mceq is None: 
         mceq = MCEqRun(
             interaction_model='SIBYLL2.3c',
             primary_model = (crf.HillasGaisser2012, 'H3a'),
             theta_deg = 0.
         )
+
+    mceq.set_density_model(a)
+
     theta = np.atleast_1d(theta)
-    fluxes = []
-    try:
-        for t in theta:
-            mceq.set_density_model(RWSAtmosphere(r))
-            mceq.set_theta_deg(t)
-            mceq.solve()
-            fluxes.append(np.copy(mceq.get_solution('total_mu-') + mceq.get_solution('total_mu+')))
-        fluxes = np.stack(fluxes, -1)
-        return r.obs, theta, fluxes
-    except ValueError as err:
-        return r.obs, theta, err
+    flux_A = []
+    depths = []
     
+    for t in theta:
+        mceq.set_theta_deg(t)
+        int_grid = None if alt_grid is None else a.h2X(np.array(alt_grid)*100.0)
+        mceq.solve(int_grid)
+        flux_X = []
+        depth  = np.empty(0, 'd')
+        if int_grid is not None:
+            depth = np.concatenate((depth, int_grid))
+            for i in range(len(int_grid)):
+                flux_X.append(np.copy(
+                    mceq.get_solution('total_mu-', grid_idx=i) + \
+                    mceq.get_solution('total_mu+', grid_idx=i)))
+
+        flux_X.append(np.copy(
+            mceq.get_solution('total_mu-') + \
+            mceq.get_solution('total_mu+')))
+        depths.append(np.concatenate((depth, (a.max_X,))))
+        flux_A.append(np.stack(flux_X, 0))
+
+        if progress is not None: progress.update(1)
+
+    return a.profile, np.stack(depths, 0), np.swapaxes(np.stack(flux_A, 0), 0, 1)
 
 if __name__ == "__main__":
     import argparse
@@ -36,39 +70,71 @@ if __name__ == "__main__":
                         help='Run number')
     parser.add_argument('N', type=int, 
                         help='Number of patterns to generate')
-    parser.add_argument('-a', '--num-angles', metavar='NA', type=int, default=4,
-                        help='Number of angular segments between 0 and 90 inclusive')
+    parser.add_argument('-b', '--barometer', type=float, default=29.92)
+    parser.add_argument('-t', '--theta', nargs='+', type=float, default=4,
+                        help='List of theta angles (degrees) to simulate')
     parser.add_argument('-m', '--multiprocess', metavar='M', type=int, default=1,
                         help='Number of processes to use (default=1 no multiprocessing)')
     parser.add_argument('-p', '--prefix', type=str, default='ATM')
+    parser.add_argument('-u', '--progress', action='store_true', default=False,
+                        help='Display tqdm progress bar.')
     parser.add_argument('--e-min', metavar='E0', type=float, default=1.0)
     parser.add_argument('--e-max', metavar='E1', type=float, default=1E10)
+    parser.add_argument('-H', '--alt-grid', nargs='+', type=float)
 
     args = parser.parse_args()
 
     filename = f'{args.prefix}{args.run:06d}.npz'
-    theta = np.linspace(0, 90, args.num_angles, 'd')
+    theta = np.array(args.theta, 'd')
     mceq_config.e_min = args.e_min
     mceq_config.e_max = args.e_max
+    P0 = args.barometer * 3386.53075
+    progress = None
 
-    if args.multiprocess > 1:
-        from concurrent.futures import ProcessPoolExecutor
-        with ProcessPoolExecutor(max_workers=args.multiprocess) as exe:
-            futures = []
+    try:
+        if args.multiprocess > 1:
+            from concurrent.futures import ProcessPoolExecutor
+            with ProcessPoolExecutor(max_workers=args.multiprocess) as exe:
+                futures = []
+                for i in range(args.N):
+                    futures.append(exe.submit(
+                        simulate_atmosphere, 
+                        generate_atmosphere(P0 = P0), theta))
+
+            res = [f.result() for f in futures]
+        else:
+            mceq = MCEqRun(
+                interaction_model='SIBYLL2.3c',
+                primary_model = (crf.HillasGaisser2012, 'H3a'),
+                theta_deg = 0.
+            )
+            if args.progress:
+                from tqdm import tqdm
+                progress = tqdm(total=args.N*len(theta))
+        
+            res = []
             for i in range(args.N):
-                futures.append(exe.submit(simulate_atmosphere, RWSim(), theta))
+                try:
+                    a = generate_atmosphere(P0 = P0)
+                    res.append(
+                        simulate_atmosphere(
+                        a, theta, mceq=mceq, 
+                        alt_grid=args.alt_grid, 
+                        progress=progress))
+                except ValueError as err:
+                    print(f'Caught Error on Event #{i}:', err)
+                    
+    except KeyboardInterrupt:
+        print(f'Caught interrupt - terminating early with {len(res)} atmospheres.')
+    finally:        
 
-        pat = [f.result() for f in futures]
-    else:
-        mceq = MCEqRun(
-            interaction_model='SIBYLL2.3c',
-            primary_model = (crf.HillasGaisser2012, 'H3a'),
-            theta_deg = 0.
-        )
-        pat = [simulate_atmosphere(RWSim(), theta, mceq=mceq) for i in range(args.N)]
+        atm, ig, flux = list(zip(*res))
+        atm = np.stack(atm, 0)
+        ig  = np.stack(ig, 0)
+        flux = np.stack(flux, 0)
 
-    atm, theta, flux = list(zip(*pat))
-    atm = np.stack(atm, -1)
-    flux = np.stack(flux, -1)
-    np.savez(filename, e_grid=mceq.e_grid, theta=theta[0], atm=atm, flux=flux)
+        np.savez(filename, e_grid=mceq.e_grid, 
+                 pressure=np.array((P0,), 'd'),
+                 int_grid=ig, alt_grid=np.array(args.alt_grid, 'd'),
+                 theta=theta, atm=atm, flux=flux)
 
